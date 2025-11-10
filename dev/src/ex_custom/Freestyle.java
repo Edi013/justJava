8b80ecfc1c9d58d96222230c4667cc309986409a
Edi013 <edymare97@yahoo.com>
2025-11-10 22:39:30 +0200

    Section 1 from Java Practice Exercies

--- dev/src/ex_custom/Freestyle.java
+++ dev/src/ex_custom/Freestyle.java
@@ -4,4 +4,20 @@ public class Freestyle {
     public static void main(){
 
     }
+
+    public static void TestFoundation1(){
+        int x;
+        boolean bool;
+        if(true){} // no compile or runtime errors
+        // if(x=3){} // compile error - numbers not allowed/evaluated as boolean
+        if (bool = false) {}
+
+    }
+
+    public static void TestFoundation2(){
+        int i = 1234567890;
+        float f = i; //autoboxing
+
+        // class MyString extends String{    MyString(){ super(); } } - doesn t compile to extend final class String
+    }
 }
